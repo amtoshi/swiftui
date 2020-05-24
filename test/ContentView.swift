ceb799dfd7e6e9ad7e24f4dd47ee72932d116112
Ashutosh Mane <ashutosh@blabby.io>
2020-05-24 11:24:39 +0530

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Ashutosh Mane on 24/05/20.
+//  Copyright © 2020 Ashutosh Mane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
